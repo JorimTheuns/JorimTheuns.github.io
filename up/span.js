46f39786ff480cd7df976a3b9aa23d0013b426e9
Jorim <johannestheuns@gmail.com>
2019-05-16 18:56:08 +0200

    small adjustments

--- up/span.js
+++ up/span.js
@@ -1,10 +1,10 @@
 $(function() {
-            $('h1, h2, p, blockquote, a').wrapInner("<span></span>");
+            $('h1, h2, h3, p, blockquote, a').wrapInner("<span></span>");
         });
 
 
         $(function() {
-            $('div').not(".grid-container, .hover-over, .hover-over-content, .aspect-ratio, .filler, .main-grid, .variant-four").hover(
+            $('div').not(".desc, .grid-container, .hover-over, .hover-over-content, .aspect-ratio, .filler, .main-grid, .variant-four").hover(
                 function() {
                     $(this).addClass("skew")
                 },
